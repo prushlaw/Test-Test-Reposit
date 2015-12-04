03f34473c04b5a3dab6e47243e88d077e9dfad1c
Pat <Pat@Pat-PC.attlocal.net>
2015-12-03 20:51:04 -0500

    Following GitHub Assignment instructions

---
+++ GitHub Assignment/src/github.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Pat
+ *
+ */
+public class github {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
